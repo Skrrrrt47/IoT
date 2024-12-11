9eebdbd1f19f957efcd085fa4956b90250d9a0b4
Thomas Eychenne <thomas.eychenne@epita.fr>
2024-12-11 15:04:08 +0100

    feat: adding delete data on stop 2

--- backend/src/observers/add-data.observer.ts
+++ backend/src/observers/add-data.observer.ts
@@ -34,39 +34,41 @@ export class AddDataObserver implements LifeCycleObserver {
    */
   async start(): Promise<void> {
     // Add your logic for start
-    let tables: Table[] = [
-      new Table({
-        status: true,
-        capacity: 10,
-        dateMaintenance: new Date().toISOString(),
-      }),
-      new Table({
-        status: true,
-        capacity: 20,
-        dateMaintenance: new Date().toISOString(),
-      }),
-    ];
+    if ((await this.salesRepo.count()).count !== 0) {
+      let tables: Table[] = [
+        new Table({
+          status: true,
+          capacity: 10,
+          dateMaintenance: new Date().toISOString(),
+        }),
+        new Table({
+          status: true,
+          capacity: 20,
+          dateMaintenance: new Date().toISOString(),
+        }),
+      ];
 
-    let beers: Beer[] = [
-      new Beer({
-        name: 'Pilsner',
-        description: 'A light, crisp and bitter beer with a dry finish.',
-        type: 'Pilsner',
-        price: 5,
-        urlImg:
-          'https://upload.wikimedia.org/wikipedia/commons/d/da/Pilsner_urquell_mug.jpg',
-      }),
-      new Beer({
-        name: 'IPA',
-        description: 'A fruity and hoppy beer with a higher alcohol content',
-        type: 'IPA',
-        price: 6,
-        urlImg:
-          'https://upload.wikimedia.org/wikipedia/commons/3/32/Fuller%27s_India_pale_ale.jpg',
-      }),
-    ];
-    this.salesRepo.createAll(tables);
-    this.beerRepo.createAll(beers);
+      let beers: Beer[] = [
+        new Beer({
+          name: 'Pilsner',
+          description: 'A light, crisp and bitter beer with a dry finish.',
+          type: 'Pilsner',
+          price: 5,
+          urlImg:
+            'https://upload.wikimedia.org/wikipedia/commons/d/da/Pilsner_urquell_mug.jpg',
+        }),
+        new Beer({
+          name: 'IPA',
+          description: 'A fruity and hoppy beer with a higher alcohol content',
+          type: 'IPA',
+          price: 6,
+          urlImg:
+            'https://upload.wikimedia.org/wikipedia/commons/3/32/Fuller%27s_India_pale_ale.jpg',
+        }),
+      ];
+      this.salesRepo.createAll(tables);
+      this.beerRepo.createAll(beers);
+    }
   }
 
   /**
@@ -74,7 +76,5 @@ export class AddDataObserver implements LifeCycleObserver {
    */
   async stop(): Promise<void> {
     // Add your logic for stop
-    this.salesRepo.deleteAll();
-    this.beerRepo.deleteAll();
   }
 }
