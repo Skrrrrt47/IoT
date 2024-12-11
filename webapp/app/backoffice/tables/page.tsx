3a61a6f1e6397426e2a211da75ce135b08d0f8ed
mikabanana <breton.arthur@orange.fr>
2024-12-11 17:20:25 +0100

    Feat : Financial dashboard imporivements

--- webapp/app/backoffice/tables/page.tsx
+++ webapp/app/backoffice/tables/page.tsx
@@ -1,84 +1,124 @@
 "use client";
-import React, { use } from 'react'
+import React, { use } from "react";
 import { useSearchParams } from "next/navigation";
-import StatBox from '@/app/components/StatBox';
-import { Beer, Command } from '@/app/types/type';
-import { useState,useEffect } from 'react';
-
+import StatBox from "@/app/components/StatBox";
+import { Beer, Command } from "@/app/types/type";
+import { useState, useEffect } from "react";
 
 function TableDetails() {
+  const searchParams = useSearchParams();
+  const tableId = searchParams.get("tableId");
+  const [commands, setCommands] = useState<Command[] | null>(null);
 
-    const searchParams = useSearchParams();
-    const tableId = searchParams.get("tableId");
-    const [commands, setCommands] = useState<Command[] | null>(null);
+  const [total, setTotal] = useState(0);
+  const [capacity, setCapacity] = useState(0);
+  const [popularBeer, setPopularBeer] = useState<Beer | null>(null);
+  const [nbTables, setNbTables] = useState(0);
 
-    const [total, setTotal] = useState(0);
-    const [capacity, setCapacity] = useState(0);
-    const [popularBeer, setPopularBeer] = useState<Beer | null>(null);
+  function getMostFrequentBeerId(commands: Command[]): number | null {
+    const beerCounts: Record<number, number> = {};
+    for (const command of commands) {
+      beerCounts[command.beerId] = (beerCounts[command.beerId] || 0) + 1;
+    }
+    let maxCount = 0;
+    let mostFrequentBeerId: number | null = null;
 
-    function getMostFrequentBeerId(commands: Command[]): number | null {
-        const beerCounts: Record<number, number> = {};
-        for (const command of commands) {
-          beerCounts[command.beerId] = (beerCounts[command.beerId] || 0) + 1;
-        }
-        let maxCount = 0;
-        let mostFrequentBeerId: number | null = null;
-      
-        for (const [beerId, count] of Object.entries(beerCounts)) {
-          if (count > maxCount) {
-            maxCount = count;
-            mostFrequentBeerId = Number(beerId);
-          }
-        }
-      
-        return mostFrequentBeerId;
+    for (const [beerId, count] of Object.entries(beerCounts)) {
+      if (count > maxCount) {
+        maxCount = count;
+        mostFrequentBeerId = Number(beerId);
+      }
     }
 
+    return mostFrequentBeerId;
+  }
 
-    useEffect(() => {
-        const fetchCommands = async () => {
-            try {
-                const response = await fetch(`http://localhost:3001/commands/`);
-                const data = await response.json();
-                const filteredData = data.filter((command: Command) => command.tableId === parseInt(tableId!));
-                setCommands(filteredData);
-            } catch (error) {
-                console.error("Error fetching commands:", error);
-            }
+  useEffect(() => {
+    const fetchCommands = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/commands/`);
+        const data = await response.json();
+        const filteredData = data.filter(
+          (command: Command) => command.tableId === parseInt(tableId!)
+        );
+        setCommands(filteredData);
+      } catch (error) {
+        console.error("Error fetching commands:", error);
+      }
+    };
+    fetchCommands();
+  }, [tableId]);
+
+  useEffect(() => {
+    const fetchCommands = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/tables/count`);
+        const data = await response.json();
+        setNbTables(data.count);
+      } catch (error) {
+        console.error("Error fetching commands:", error);
+      }
+    };
+    fetchCommands();
+  }, [nbTables]);
+
+  useEffect(() => {
+    if (commands) {
+      console.log(commands);
+      const total = commands.reduce((acc, command) => acc + command.price, 0);
+      setTotal(total);
+      const capacity = commands.reduce(
+        (acc, command) => acc + command.nbBeers,
+        0
+      );
+      setCapacity(capacity);
+      const mostFrequentBeerId = getMostFrequentBeerId(commands);
+      if (mostFrequentBeerId) {
+        const fetchPopularBeer = async () => {
+          const response = await fetch(
+            `http://localhost:3001/beers/${mostFrequentBeerId}`
+          );
+          const data = await response.json();
+          setPopularBeer(data);
         };
-        fetchCommands();
-    }, [tableId]);
-
-    useEffect(() => {
-        if (commands) {
-            console.log(commands);
-            const total = commands.reduce((acc, command) => acc + command.price, 0);
-            setTotal(total);
-            const capacity = commands.reduce((acc, command) => acc + command.nbBeers, 0);
-            setCapacity(capacity);
-            const mostFrequentBeerId = getMostFrequentBeerId(commands);
-            if (mostFrequentBeerId) {
-                const fetchPopularBeer = async () => {
-
-                    const response = await fetch(`http://localhost:3001/beers/${mostFrequentBeerId}`);
-                    const data = await response.json();
-                    setPopularBeer(data);
-                }
-                fetchPopularBeer();
-            }
-        }
-    },[commands]);
+        fetchPopularBeer();
+      }
+    }
+  }, [commands]);
 
   return (
-    <div className='min-h-screen flex flex-col justify-start items-center bg-gray-700'>
-        <h1 className='text-2xl font-bold mb-4 text-white'>Table Details n°{tableId}</h1>
-        <div className='flex flex-row gap-10'>
-        <StatBox icon={"/circle.svg"} title='CA Total' value={total} />
-        <StatBox icon={"/circle.svg"} title='Numbers of beer served' value={capacity} />
-        <StatBox icon={"/circle.svg"} title='Most Popular Beer' value={popularBeer?.name ?? ""} />
-        </div>
+    <div className="min-h-screen flex flex-col justify-start items-center bg-gray-700">
+      <h1 className="text-2xl font-bold mb-4 text-white">
+        Table Details n°{tableId}
+      </h1>
+      <div className="flex flex-row items-center gap-10">
+        <img
+          className="w-10 h-10 inline-block align-middle"
+          src={"/arrow-bar-left-svgrepo-com.svg"}
+        />
+        <StatBox icon={"/circle.svg"} title="CA Total" value={total} />
+        <StatBox
+          icon={"/circle.svg"}
+          title="Numbers of beer served"
+          value={capacity}
+        />
+        <StatBox
+          icon={"/circle.svg"}
+          title="Most Popular Beer"
+          value={popularBeer?.name ?? ""}
+        />
+        <StatBox
+          icon={"/circle.svg"}
+          title="Operational Tables"
+          value={nbTables ? nbTables : ""}
+        />
+        <img
+          className="w-10 h-10 inline-block align-middle"
+          src={"/arrow-right-line.svg"}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default TableDetails
+export default TableDetails;
--- webapp/app/components/StatBox.tsx
+++ webapp/app/components/StatBox.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const StatBox = ({ icon, title, value } : {icon: string,title: string, value: string | number}) => {
+const StatBox = ({
+  icon,
+  title,
+  value,
+}: {
+  icon: string;
+  title: string;
+  value: string | number;
+}) => {
   return (
-    <div className="bg-white shadow rounded-lg p-4 items-center text-center min-w-40">
+    <div className="bg-white shadow rounded-lg p-4 flex flex-col items-center text-center min-w-40">
       <img className="w-10 h-10" src={icon} />
-      <h3 className="text-lg font-semibold">{value}</h3>
+      <h3 className="text-gray-600 font-semibold">{value}</h3>
       <p className="text-sm text-gray-600">{title}</p>
     </div>
   );
